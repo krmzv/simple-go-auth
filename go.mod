d41c05b94b57aff1a9baf9ed535daaf4eba9f7bd
krmzv <mihajlo.jovanovic@insidetrack.com>
2023-03-01 22:42:53 +0100

    Test, login, etc

--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module github.com/krmzv/reversejs
 go 1.20
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang-jwt/jwt/v5 v5.0.0-rc.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/lib/pq v1.10.7 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- types.go
+++ types.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Role struct {
@@ -19,24 +21,35 @@ var (
 	Admin     = Role{"admin"}
 )
 
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 type CreateUserRequest struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 type User struct {
 	ID        int       `json:"id"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
+	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"createdAt"`
 	Role      Role      `json:"role"`
 }
 
-func NewUser(name, email string) *User {
-
+func NewUser(name, email, password string) (*User, error) {
+	pw, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
 	return &User{
 		Name:      name,
 		Email:     email,
+		Password:  string(pw),
 		CreatedAt: time.Now().UTC(),
-	}
+	}, nil
 }
